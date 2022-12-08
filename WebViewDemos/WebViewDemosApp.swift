6cee662d6ae2b833a5a3f638527f71b4b3912ca6
Mark Norgren <mark@marknorgren.com>
2022-12-08 10:15:25 -0600

    Initial Commit

---
+++ WebViewDemos/WebViewDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewDemosApp.swift
+//  WebViewDemos
+//
+//  Created by Mark Norgren on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WebViewDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
